83c7db022dfd11798ed043f5521bbc8e167aa80f
Nhan Tri Danh <dnhan1707@gmail.com>
2023-12-30 13:27:56 -0800

    Handler Error

--- index.js
+++ index.js
@@ -32,40 +32,48 @@ app.use(express.static("public"));
 //Endpoints
 app.get("/", (req, res) => {
     res.render("main.ejs");
-})
+});
 
-app.post("/upload", upload.single("excelFile"), (req, res) => {
-    if(req.file){
-        
-        const uploadedFile = req.file.buffer;
+app.post("/upload", upload.single("excelFile"), async (req, res) => {
+    const uploadedFile = req.file.buffer;
 
+    try {
         const workbook = xlsx.read(uploadedFile);
-        let workbook_sheet = workbook.SheetNames;                
-        let workbook_response = xlsx.utils.sheet_to_json(        
+        const workbook_sheet = workbook.SheetNames;
+        const workbook_response = xlsx.utils.sheet_to_json(
             workbook.Sheets[workbook_sheet[0]]
         );
 
-        workbook_response.forEach(row => {
+        for (const row of workbook_response) {
             const name = row['Full Name'];
             const recipientEmail = row['Email'];
 
-            //Insert customers' contacts
-            try
-            {
-                db.query(
-                "INSERT INTO customer_contacts (name, email) VALUES ($1, $2)",
-                [name, recipientEmail]
-                ) 
-            }catch(err)
-            {
-                console.log(`Email: ${recipientEmail} already exists`)
+            // Insert customers' contacts
+            try {
+                await db.query(
+                    "INSERT INTO customer_contacts (name, email) VALUES ($1, $2)",
+                    [name, recipientEmail]
+                );
+            } catch (err) {
+                if (err.code === '23505') {
+                    console.log(`Email: ${recipientEmail} already exists`);
+                } else {
+                    console.error("Error inserting into the database:", err);
+                }
             }
+        }
 
-        });
+        // If all inserts were successful, redirect
+        res.redirect("/");
 
-    res.redirect("/");
+    } catch (err) {
+        console.error("Error processing the uploaded file:", err);
+        res.render("main.ejs", { error: "Error processing the file" });
     }
-})
+});
+
+
+
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
