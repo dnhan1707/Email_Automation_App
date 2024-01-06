d5d1393f52de383c6f2188964af4bbc84f781a64
Nhan Tri Danh <dnhan1707@gmail.com>
2024-01-05 18:17:23 -0800

    Implemented Database PostgreSQL
    Need to handle edge cases

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get("/", async (req, res) => {
     res.render("main.ejs");
 });
 
-
+    
 app.post("/compose_email", async (req, res) => {
     res.render("compose_email.ejs");
 });
@@ -75,43 +75,56 @@ async function process_contact_file(uploadedFile, subject, modifiedHtmlContent, 
 {
 
     try {
+        const base64content = imageDataArray[0].Base64Content;
         const workbook = xlsx.read(uploadedFile);
         const workbook_sheet = workbook.SheetNames;
         const workbook_response = xlsx.utils.sheet_to_json(
             workbook.Sheets[workbook_sheet[0]]
         );
 
+
+        ///INSERT DATA INTO POSTGRESQL
+        //Insert email
+        const timestamp = new Date();
+
+        try {
+            await db.query(
+                "INSERT INTO email (sender_email, subject, body, sent_at, image_data)  VALUES ($1, $2, $3, $4, $5)",
+                [process.env.SENDER_EMAIL, subject, modifiedHtmlContent, timestamp, base64content]
+            )
+        } catch (error) {
+            console.error("Error inserting into the email table:", error);
+        }
+
+        // Insert into the record table
+        const emailIdResult = await db.query(
+            "SELECT email_id FROM email ORDER BY email_id DESC LIMIT 1"
+        );
+        const emailId = emailIdResult.rows[0].email_id;
+
         for (const row of workbook_response) {
             const name = row['Full Name'];
             const recipientEmail = row['Email'];
 
-            await process_email(name, recipientEmail, subject, modifiedHtmlContent, imageDataArray);
-
             // Insert customers' contacts
-            // try {
-            //     await db.query(
-            //         "INSERT INTO customer_contact (name, email_address) VALUES ($1, $2)",
-            //         [name, recipientEmail]
-            //     );
-            // } catch (err) {
-            //     if (err.code === '23505') {
-            //         console.log(`Email: ${recipientEmail} already exists`);
-            //     } else {
-            //         console.error("Error inserting into the database:", err);
-            //     }   
-            // }
-        }
-        // const timestamp = new Date();
+            try {
+                await db.query(
+                    "INSERT INTO customer_contact (name, email_address) VALUES ($1, $2)",
+                    [name, recipientEmail]
+                );
 
-        // //Insert email history
-        // try {
-        //     await db.query(
-        //         "INSERT INTO email (sender_email, subject, body, sent_at, image_data)  VALUES ($1, $2, $3, $4, $5)",
-        //         ["dnhan1707@gmail.com", subject, modifiedHtmlContent, timestamp, stringifiedImageDataArray]
-        //     )
-        // } catch (error) {
-        //     console.error("Error inserting into the email table:", error);
-        // }
+            } catch (err) {
+                if (err.code === '23505') {
+                    console.log(`Email: ${recipientEmail} already exists`);
+                } else {
+                    console.error("Error inserting into the database:", err);
+                }   
+            }
+
+            await process_email(emailId, name, recipientEmail, subject, modifiedHtmlContent, imageDataArray);
+        }
+
+
 
     } catch (err) {
         console.error("Error processing the uploaded file:", err);
@@ -120,7 +133,7 @@ async function process_contact_file(uploadedFile, subject, modifiedHtmlContent, 
 }
 
 
-async function process_email(recipientName, recipientEmail, subject, html_part, imageDataArray) {
+async function process_email(emailId, recipientName, recipientEmail, subject, html_part, imageDataArray) {
     try {
         let message = {
             From: {
@@ -154,6 +167,25 @@ async function process_email(recipientName, recipientEmail, subject, html_part, 
                 console.log("Could not send email");
                 throw err;
             });
+        
+        const customerIdResult = await db.query(
+            "SELECT customer_id FROM customer_contact WHERE email_address = $1",
+            [recipientEmail]
+        )
+        try {
+            const customerId = customerIdResult.rows[0].customer_id;
+
+            // Insert into the history table
+            await db.query(
+                "INSERT INTO record (email_id, customer_id) VALUES ($1, $2)",
+                [emailId, customerId]
+            );
+
+            console.log(result.body);
+        } catch (err) {
+            console.error(`Customer with email ${recipientEmail} not found.`);
+        }
+
     } catch (err) {
         console.error("Error in process_email function:", err);
         throw err;
